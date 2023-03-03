36863998933869f39f2c317fc1502fb03dfa21ae
Марс <profimars@mail.ru>
2023-03-03 11:29:03 +0300

    Добавил задачу 15

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 15
+//Напишите програамму, которая принимает на вход цифру, обозначающую день недели,  
+//и проверяет является ли этот день выходным
+//6 -> да
+//7 -> да
+//1 -> нет
+//https://github.com/profimars/C_SharpTask15
+Console.WriteLine("Ведите целое положительное число от 1 до 7");
+int q = Convert.ToInt32(Console.ReadLine());
+if (q >= 1 && q <= 7)
+{
+    if (q == 6 || q == 7)
+        Console.WriteLine("да");
+    else
+        Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("Вы ввели неправильное число");
+}
